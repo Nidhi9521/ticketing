b32777143aab254db64c64f694df5e83d9057931
nidhi.gokani <gokaninidhi9521@gmail.com>
2022-10-12 16:52:03 +0530

    some change

--- auth/src/routes/_test_/current-user.test.ts
+++ auth/src/routes/_test_/current-user.test.ts
@@ -1,16 +1,16 @@
 import request from 'supertest'
 import { app } from '../../app'
 
-it('responds with details about the current user',async () =>{
+it('responds with details about the current user', async () => {
 
 
-    const cookie=await global.signin();
+    const cookie = await global.signin();
 
     const response = await request(app)
-    .get('/api/users/currentuser')
-    .set('Cookie',cookie)
-    .send()
-    .expect(200)
+        .get('/api/users/currentuser')
+        .set('Cookie', cookie)
+        .send()
+        .expect(200)
 
     expect(response.body.currentUser.email).toEqual('test@test.com')
     console.log(response.body);
@@ -18,9 +18,12 @@ it('responds with details about the current user',async () =>{
 })
 
 
-it('responds with null if not authenticated',async()=>{
+it('responds with null if not authenticated', async () => {
     const response = await request(app)
-    .get('api/users/currentuser')
-    .send({})
-    .expect(404);
+        .get('api/users/currentuser')
+        .send({})
+        .expect(404);
+    expect(response.statusCode).toStrictEqual(404)
+    console.log('response', response.statusCode);
+
 })
